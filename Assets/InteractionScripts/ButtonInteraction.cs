1f0007d24ca26615782fb207d85fefad123f1361
Tzu Hsuan Huang <thhuang1@uci.edu>
2023-05-09 21:50:24 -0700

    Finish basic object interaction

---
+++ Assets/InteractionScripts/ButtonInteraction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteraction : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string text = "(E) Open Door";
+    public string interactionText=>text;
+    public bool Interact(Interactor interactor){
+        Debug.Log("Pushing button!");
+        return true;
+    }
+}
