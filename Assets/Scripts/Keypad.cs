11f04c5698c2b90025adb2d66e5d8abc9d726394
DragonTamer13 <wynekids@yahoo.com>
2023-05-10 20:11:19 -0700

    Keypad

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private Text Ans;
+
+    private string Answer = "123456";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+    public void Execute()
+    {
+        if (Ans.text == Answer)
+        {
+            Ans.text = "Correct";
+        }
+        else
+        {
+            Ans.text = "Wrong";
+        }
+    }
+}
