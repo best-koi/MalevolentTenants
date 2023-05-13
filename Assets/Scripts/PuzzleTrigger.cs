a0960efc0dc06cb16b8f5ed168f55057fe3d694d
jcperez1106 <jancp@uci.edu>
2023-05-12 18:24:36 -0700

    Game Demo
    Fix the game

---
+++ Assets/Scripts/PuzzleTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PuzzleTrigger : MonoBehaviour
+{
+    public bool solved = false;
+    void OnTriggerEnter (Collider other)
+    {
+        solved = true;
+        Debug.Log("Door Unlocked!");
+    }
+}
