ec8070ab3e73b045a1913d46f9e5dae411049297
Tzu Hsuan Huang(Alyssa) <117048389+allyer88@users.noreply.github.com>
2023-05-17 17:00:58 -0700

    fix

--- Assets/InteractionScripts/Interactor.cs
+++ Assets/InteractionScripts/Interactor.cs
@@ -10,9 +10,11 @@ public class Interactor : MonoBehaviour
     [SerializeField] private int numFound;
     [SerializeField] private TextInteraction textinteraction;
     private IInteractable interactableItem;
+    private ReadInteraction readinteraction;
     private Collider[] colliders=new Collider[3]; //If overlap, only three items can interact
     private void Update() {
         numFound= Physics.OverlapSphereNonAlloc(interactionPoint.position, interactionRadius, colliders, InteractableLayer);
+        readinteraction = colliders[0].GetComponent<ReadInteraction>();
         if(numFound>0){
             interactableItem = colliders[0].GetComponent<IInteractable>();
             if(interactableItem!=null){
--- Assets/InteractionScripts/TextInteraction.cs
+++ Assets/InteractionScripts/TextInteraction.cs
@@ -8,14 +8,23 @@ public class TextInteraction : MonoBehaviour
     private Camera currentCam;
     [SerializeField] private GameObject uiPanel;
     [SerializeField] private TextMeshProUGUI promptText;
+    GameObject player;
     private void Start() {
         uiPanel.SetActive(false);
+        player= GameObject.FindWithTag("Player");
     }
     private void Update() {
         //Let the text rotate with the camera
         currentCam=Camera.main;
-        var rotation= currentCam.transform.rotation;
-        transform.LookAt(transform.position+rotation*Vector3.forward, rotation * Vector3.up);
+        uiPanel.transform.LookAt(uiPanel.transform.position + currentCam.transform.rotation * Vector3.forward,
+                             currentCam.transform.rotation * Vector3.up);
+        // Check if the player object is found
+        if (player != null)
+        {
+            Vector3 playerPosition = player.transform.position;
+            // Follow the player's movement in the X and Z axes while keeping the Y value
+            transform.position = new Vector3(playerPosition.x, transform.position.y, playerPosition.z);
+        }
     }
     public bool IsDisplayed=false;
     public void SetUp(string theText){
